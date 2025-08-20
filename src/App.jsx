a2de3d3ec896556968d26b0317b27ffe0cae844a
Shruti <shrutinavale29@gmail.com>
2025-08-20 14:43:36 +0530

    feat: added archive page functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { Important } from './pages/Important';
+import { Archive } from './pages/Archive';
 
 function App() {
   console.log("App is rendering...");
@@ -10,6 +11,7 @@ function App() {
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/important' element={<Important/>}/>
+      <Route path='/archive' element={<Archive/>}/>
     </Routes>
   )
 };
